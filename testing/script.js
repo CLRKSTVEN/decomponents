9a6ecdbe45dccecf1733f7f8a25ea9af56909c22
Clark Steven <edongclarksteven@gmail.com>
2025-12-01 18:05:54 +0800

    commit

---
+++ testing/script.js
@@ -0,0 +1,33 @@
+const signUpButton=document.getElementById('signUpButton');
+const signInButton=document.getElementById('signInButton');
+const signInForm=document.getElementById('signIn');
+const signUpForm=document.getElementById('signup');
+
+signUpButton.addEventListener('click',function(){
+    signInForm.style.display="none";
+    signUpForm.style.display="block";
+})
+signInButton.addEventListener('click', function(){
+    signInForm.style.display="block";
+    signUpForm.style.display="none";
+})
+
+const profileImage = document.getElementById('profile_image');
+const imagePreview = document.getElementById('image-preview');
+
+profileImage.addEventListener('change', function() {
+    const file = profileImage.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            imagePreview.src = e.target.result;
+            imagePreview.style.display = 'block';
+        }
+
+        reader.readAsDataURL(file);
+    } else {
+        imagePreview.style.display = 'none';
+    }
+});
